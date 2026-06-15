ffd9e56721273d19b11c72c75d93bd37ddf5838f
VSDragon1239 <iliamalcev31119@mail.ru>
2026-06-15 23:22:58 +0900

    ct1_v0.0.24 edit settings.py

--- MarketplaceGreenHabits/settings.py
+++ MarketplaceGreenHabits/settings.py
@@ -31,7 +31,8 @@ DEBUG = True
 # )
 #
 hosts = os.getenv(
-    'DJANGO_ALLOWED_HOSTS', 'greenhabits.site,www.greenhabits.site',
+    'DJANGO_ALLOWED_HOSTS',
+    'localhost,127.0.0.1,[::1],194.87.214.67,192.168.100.196,10.0.2.2,10.23.120.101,greenhabits.site,www.greenhabits.site',
 )
 ALLOWED_HOSTS = hosts.split(',')
 
